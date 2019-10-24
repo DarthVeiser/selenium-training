a1a7311e0703192b20183eaf69f8d368b2db4018
Aleksandr Vyayzya <alex.vyayzya@gmail.com>
2019-10-24 22:06:24 +0700

    add browser firefox

--- first-task/src/test/java/LoginScenario.java
+++ first-task/src/test/java/LoginScenario.java
@@ -4,31 +4,36 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class LoginScenario {
     private WebDriver driver;
+    private WebDriver driverChrome;
 
     @Before
     public void initializeDriver() {
-        driver = new ChromeDriver();
-    }
+        driverChrome = new ChromeDriver();
+        driver = new FirefoxDriver();
+   }
 
     @After
     public void stopDriver() {
+        driverChrome.quit();
         driver.quit();
     }
 
     @Test
     public void loginForAdmin() throws InterruptedException {
+        driverChrome.get("http://localhost/litecart/admin/login.php");
         driver.get("http://localhost/litecart/admin/login.php");
 
         //Enter login and password
+        driverChrome.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("username")).sendKeys("admin");
+        driverChrome.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
 
+        driverChrome.findElement(By.xpath("//button[@name='login']")).click();
         driver.findElement(By.xpath("//button[@name='login']")).click();
         Thread.sleep(3000);
     }
