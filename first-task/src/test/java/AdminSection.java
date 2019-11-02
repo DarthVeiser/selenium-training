9a8764666b2fb7034a35b17215c55b785e36b0ea
Aleksandr Vyayzya <alex.vyayzya@gmail.com>
2019-11-02 22:11:03 +0700

    delete unused import

--- first-task/src/test/java/AdminSection.java
+++ first-task/src/test/java/AdminSection.java
@@ -5,10 +5,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
