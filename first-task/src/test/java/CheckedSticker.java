c706a9ad19ddf9796dcb8de30c288fc219cbb8c9
Aleksandr Vyayzya <alex.vyayzya@gmail.com>
2019-11-10 17:26:52 +0700

    fix selector

--- first-task/src/test/java/CheckedSticker.java
+++ first-task/src/test/java/CheckedSticker.java
@@ -25,13 +25,13 @@ public class CheckedSticker {
     public void checkSticker() throws InterruptedException {
         driverChrome.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         int products = driverChrome.findElements(By.cssSelector("div#box-campaign-products " +
-                "div.product.column.shadow.hover-light")).size();
+                "div.product")).size();
 
         int product = 1;
         while (product <= products) {
             Thread.sleep(1000);
             WebElement sticker = driverChrome.findElement(By.cssSelector("div#box-campaign-products " +
-                    "div[div.product.column.shadow.hover-light]:nth-child("+ product +")"));
+                    "div.product:nth-child("+ product +")"));
             if (sticker.findElements(By.cssSelector("div[class='sticker']")).size() > 0) {
                 AssertionError assertError = new AssertionError();
                 System.out.println("FAILED. Found an item without a sticker or with more than one sticker." +assertError.getMessage());
